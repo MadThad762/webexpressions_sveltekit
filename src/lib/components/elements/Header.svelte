a9ed0ade436d78fc2961b4b8c690277aa3e3a1e6
Thaddeus Krugman <thaddeuskrugman@gmail.com>
2023-12-18 14:32:50 -0500

    fixed spacing issue with menu X icon

--- src/lib/components/elements/Header.svelte
+++ src/lib/components/elements/Header.svelte
@@ -55,14 +55,18 @@
 			<!-- Background backdrop, show/hide based on slide-over state. -->
 			<div class="fixed inset-0 z-10"></div>
 			<div
-				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-4 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
 			>
 				<div class="flex items-center justify-between">
-					<a href="/" class="-m-1.5 p-1.5">
+					<a href="/" class="-m-1.5 px-1.5">
 						<span class="sr-only">Your Company</span>
 						<img class="h-8 w-auto" src={logo} alt="webexpressions logo" />
 					</a>
-					<button type="button" on:click={() => toggleMenu()} class="-m-2.5 rounded-md p-2.5">
+					<button
+						type="button"
+						on:click={() => toggleMenu()}
+						class="-m-2.5 rounded-md p-1.5 lg:px-2.5"
+					>
 						<span class="sr-only">Close menu</span>
 						<svg
 							class="h-10 w-10 text-heading"
