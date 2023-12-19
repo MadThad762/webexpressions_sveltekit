7834d3089606321de3891ab5ca63c7971d2137ad
Thaddeus Krugman <thaddeuskrugman@gmail.com>
2023-12-19 14:26:41 -0500

    added meta data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,12 @@
 	import '../app.css';
 </script>
 
-<!-- todo: add meta data, title, and description -->
+<svelte:head>
+	<meta name="theme-color" content="#ffffff" />
+	<meta name="twitter:image" content="webexpressions.io/og.png" />
+	<meta name="og:image" content="webexpressions.io/og" />
+</svelte:head>
+
 <div class="min-h-screen flex flex-col h-full w-full mx-auto justify-between">
 	<div class="flex flex-col w-full h-full mx-auto">
 		<Header />
